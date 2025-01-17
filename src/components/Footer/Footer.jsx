25fc91cf0a3ea85a9f6377f6fa5042251f69e228
Lembe <bruzzone123@gmail.com>
2025-01-17 16:00:03 -0500

    cambios

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,19 +9,19 @@ const Footer = () => {
         <div className="container">
           <div className="footer-row">
             <div className="footer-col">
-              <h3>Contact Us</h3>
+              <h3>CONTACTO</h3>
               <p>Email: <a href="mailto:svelez@lafelipa.com.ec">svelez@lafelipa.com.ec</a></p>
               <p>Phone: <a href="tel: +593986850872">+593 986850872</a></p>
             </div>
             <div className="footer-col">
-              <h3>Direccion</h3>
+              <h3>DIRECCIÓN</h3>
               <p><strong>Oficina:</strong>Av los Arcos 335,
                 edificio Arcos plaza 2 piso 4 oficina 423, Samborondon, 092301 Ecuador</p>
               <p><strong>Destileria: </strong>Km 8,5 via Tosagua
                 sector cerro verde Rocafuerte, Manabi. Ecuador</p>
             </div>
             <div className="footer-col">
-              <h3>Nuestras redes sociales</h3>
+              <h3>NUESTRAS REDES SOCIALES</h3>
               <div className="social-links">
                 <a href="#"><img src={assets.FacebookRosa} alt="Facebook"  /></a>
                 <a href="#"><img src={assets.InstagramRosa} alt="Instagram"  /></a>
